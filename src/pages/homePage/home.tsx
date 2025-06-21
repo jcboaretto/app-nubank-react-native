4e9728f897f94566f71d9f07c3c72d28e6c547e1
José Carlos Boaretto Netto <boarettonetto@gmail.com>
2025-06-20 21:40:25 -0300

    esqueleto feito

--- src/pages/homePage/home.tsx
+++ src/pages/homePage/home.tsx
@@ -1,15 +1,52 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { styles } from "./home-styles";
+import eu from "../../../assets/jose-nubank.png";
+import { RiBankCard2Line } from "react-icons/ri";
 
 export const Home = () => {
   return (
     <>
       <View style={styles.container}>
         <View style={styles.cima}>
+          <Image source={eu} />
           <Text style={styles.ola}> Olá, José </Text>
         </View>
-        <View style={styles.baixo}></View>
+        <View style={styles.baixo}>
+          <Text style={styles.saldo}>Saldo em conta </Text>
+          <Text style={styles.saldo}>R$ 100,00 </Text>
+
+          <View style={styles.divBolinha}>
+            <TouchableOpacity>
+              <Text style={styles.bolinhas}>Área Pix</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.bolinhas}>Pagar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.bolinhas}>Empréstimo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.bolinhas}>Caixinhas</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.bolinhas}>Recarga Celular</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.divCartoes}>
+            {/* <RiBankCard2Line /> */}
+            <TouchableOpacity>
+              <Text style={styles.cartoes}>Meus cartões</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.saldo}>Cartão de crédito</Text>
+          <Text style={styles.saldo}>Fatura atual</Text>
+          <Text style={styles.saldo}>R$ 200,00</Text>
+          <TouchableOpacity>
+            <Text style={styles.cartoes}>Cashback</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
